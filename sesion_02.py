5190b5bb6740b57edc8ada94d832c85275f01537
Scarlett-internauta <modadgonzales@gmail.com>
2026-02-12 16:21:11 -0800

    hisimos la lista y diccionarios

--- sesion_02.py
+++ sesion_02.py
@@ -1,1 +1,18 @@
+# Tuplas
+mi_tupla = (2,4)
+print("mi tupla: ",mi_tupla)
 
+# Lista
+mi_lista = [1, 3.1416, "carlos",mi_tupla]
+print("El primer elemento de la lista:",mi_lista[0])
+print("el cuarto elemento de la lista: ",mi_lista[3])
+print("el cuarto elemento de la lista: ",mi_lista[2])
+
+# Diccionarios 
+mi_diccionario = {"mi_lista": mi_lista,
+                  "nombre": "carlos",
+                  "pi": 3.1416,
+                  "tel":"313-1515300"}
+print("llave para acceder a mi diccionario de mi_lista",mi_diccionario["mi_lista"])
+print("llave para acceder a mi diccionario pi",mi_diccionario["pi"])
+print("llave para acceder a mi diccionario de tel",mi_diccionario["tel"])
